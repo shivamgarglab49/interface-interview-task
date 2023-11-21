0ec003a1795d1ac86a9d7ff08df4e576af9f6fa8
shivam_garg <shivam.garg@iongroup.com>
2023-11-21 22:52:55 +0530

    Added sorting logic based on column selection
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- src/pages/transactions/TransactionsTable.tsx
+++ src/pages/transactions/TransactionsTable.tsx
@@ -1,7 +1,8 @@
-import DataTable from "../../components/dataTable/DataTable";
-
 import { TransactionData } from "../../types";
-import { getDataColumns } from "../../utils/utils";
+import { compareFn, getDataColumns } from "../../utils/utils";
+import { useEffect, useMemo, useState } from "react";
+
+import DataTable, { SortState } from "../../components/dataTable/DataTable";
 
 interface TransactionsTableProps {
   data: TransactionData[];
@@ -10,9 +11,66 @@ interface TransactionsTableProps {
 export const TransactionsTable = ({ data }: TransactionsTableProps) => {
   if (data.length == 0) return <div />;
 
+  // transactions state
+  const [transactions, setTransactions] = useState(data);
+
+  // filters list
+  const [filters, setFilters] = useState<string[]>([]);
+
+  // sort selection state
+  const [sortState, setSortState] = useState<SortState>({
+    sortColumn: "name",
+    sortDirection: "ascending",
+  });
+
+  // handler to invoke on sort click
+  const chooseSortHandler = (selectedColumn: string) => {
+    setSortState({
+      sortColumn: selectedColumn,
+      sortDirection:
+        sortState.sortDirection === "ascending" ? "descending" : "ascending",
+    });
+  };
+
+  // handler to invoke on filter click
+  const chooseFilterHandler = (selectedColumn: string) => {
+    const items = new Set([...filters]);
+    if (items.has(selectedColumn)) {
+      items.delete(selectedColumn);
+    } else {
+      items.add(selectedColumn);
+    }
+    console.log("Items", items);
+    setFilters([...items]);
+  };
+
   // Getting all possible columns based on fist item index
-  const columns = getDataColumns(data[0]);
+  const columns = useMemo(() => getDataColumns(data[0]), [data]);
+
+  // Effect to be run when sort-state changed
+  useEffect(() => {
+    setTransactions(
+      Array.from(transactions).sort((first, second) => {
+        return compareFn(
+          sortState.sortColumn,
+          sortState.sortDirection,
+          first,
+          second
+        );
+      })
+    );
+  }, [sortState]);
 
   // Passing data and columns to component for rendering table info
-  return <DataTable<TransactionData> data={data} columns={columns} />;
+  return (
+    <DataTable<TransactionData>
+      data={transactions}
+      columns={columns}
+      filterState={filters}
+      onChooseSort={chooseSortHandler}
+      onChooseFilter={chooseFilterHandler}
+      sortState={sortState}
+      onRowClick={(data) => alert(data.name)}
+    />
+  );
 };
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,6 +7,8 @@ function capitalizeFirstLetter(input: string): string {
 
 export function getDataColumns(
   transaction: TransactionData,
+  sortingEnabled: boolean = true,
+  filterEnabled: boolean = true,
   capitalizationEnabled: boolean = true,
   nonCapitalizationList: string[] = ["sizeValueUSD"],
   hiddenColumnNameList: string[] = ["id"]
@@ -19,6 +21,8 @@ export function getDataColumns(
           !capitalizationEnabled || nonCapitalizationList.includes(header)
             ? header
             : capitalizeFirstLetter(header),
+        sortField: sortingEnabled ? header : undefined,
+        filterField: filterEnabled ? header : undefined,
         renderCell: (data: TransactionData) => {
           const headerType = typeof data[header];
           if (headerType === "boolean") {
@@ -31,4 +35,19 @@ export function getDataColumns(
         },
       };
     });
+}
+
+export function compareFn(
+  key: string,
+  sortDirection: string,
+  first: TransactionData,
+  second: TransactionData
+) {
+  if (first[key] < second[key]) {
+    return sortDirection === "ascending" ? -1 : 1;
+  }
+  if (first[key] > second[key]) {
+    return sortDirection === "ascending" ? 1 : -1;
+  }
+  return 0;
 }
