4732462c682a0e46a8e9cf9fc1b2a9b4c83d73f0
shivam_garg <shivam.garg@iongroup.com>
2023-11-21 21:57:42 +0530

    Fixed column name capitalization with PPP, sizeValueUSD columns data
    Signed-off-by: shivam_garg <shivam.garg@iongroup.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,34 @@
 import { DataColumn } from "../components/dataTable/DataTable";
 import { TransactionData } from "../types";
 
+function capitalizeFirstLetter(input: string): string {
+  return input.slice(0, 1).toUpperCase() + input.slice(1);
+}
+
 export function getDataColumns(
-  transaction: TransactionData
+  transaction: TransactionData,
+  capitalizationEnabled: boolean = true,
+  nonCapitalizationList: string[] = ["sizeValueUSD"],
+  hiddenColumnNameList: string[] = ["id"]
 ): DataColumn<TransactionData>[] {
-  return Object.keys(transaction).map((header: string) => {
-    return {
-      headerText: header,
-      renderCell: (data: TransactionData) => {
-        return "" + data[header];
-      },
-    };
-  });
+  return Object.keys(transaction)
+    .filter((header: string) => !hiddenColumnNameList.includes(header))
+    .map((header: string) => {
+      return {
+        headerText:
+          !capitalizationEnabled || nonCapitalizationList.includes(header)
+            ? header
+            : capitalizeFirstLetter(header),
+        renderCell: (data: TransactionData) => {
+          const headerType = typeof data[header];
+          if (headerType === "boolean") {
+            return data[header] === true ? "Yes" : "No";
+          } else if (headerType === "number") {
+            return data[header] === 0 ? "--" : data[header];
+          } else {
+            return data[header];
+          }
+        },
+      };
+    });
 }
